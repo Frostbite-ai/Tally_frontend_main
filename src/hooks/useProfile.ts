d4d77c05884acf0c6c5f0b98ee29fe3976c5fae7
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-07-22 00:35:24 +0530

    delete few things

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,37 +1,10 @@
-import useSWR from 'swr';
-
-import { LeaderboardPayload } from './useLeaderboard';
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-type UserPayload = {
-  name: string;
-  createdAt: string;
-};
-
-
-export const getCurrentUser = async (): Promise<UserPayload | null> => {
-  const res = await fetch(`${API_URL}/currentUser`);
-  if (!res.ok) {
-    throw new Error('An error occurred while fetching the data.');
-  }
-  return res.json();
-};
 
 
 
 const useProfile = () => {
-  const {
-    data: user,
-    isLoading,
-    mutate,
-  } = useSWR('getCurrentUser', getCurrentUser);
 
-  const { data: profileStats } = useSWR('getProfileStats');
 
-  const clearUser = () => mutate(null, false);
-
-  return { user, clearUser, isLoading, profileStats };
+  return "none";
 };
 
 export default useProfile;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextApiHandler } from 'next';
 import NextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
 
 import prisma from '@/lib/prisma';
 
@@ -12,10 +11,6 @@ export default authHandler;
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
